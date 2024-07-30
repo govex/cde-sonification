397cf8cf253308d642209dfc8242565fc5ba5cdc
Heather Bree <me@justheatherbree.com>
2024-07-30 16:33:00 -0700

    version 2 with new data and layout and user flow

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,10 +1,129 @@
 <script lang="ts">
-  import { base } from "$app/paths";
   import places from "$lib/Places.json";
-  let selected: any;
-  $: id = selected?.id;
-  $: label = selected?.PlaceDescriptions[0].display_label;
-  $: href = `${base}/sonified/${id}`;
+  import seriesvalues from "$lib/SeriesesValues.json";
+  import seriesDescriptions from "$lib/Serieses.json";
+  import { DateTime } from "luxon";
+  import { getAudioData, linearPath } from "waveform-path";
+  import { format } from "d3";
+  import * as Erie from 'erie-web';
+
+  let stopped = true;
+  let selected_place: any;
+  let series_selection:string[] = [];
+
+  // we can alter this so it's a better mapping of sound -> series_id
+  // let sounds be an object with series_id as key and filename as value 
+  // ex: { city_overview-households-totals_yearly: construction_noise.wav }
+  let sounds = [
+    "bells.wav",
+    "birdsong.wav",
+    "cardinal.mp3",
+    "industrial.mp3",
+    "keyboard-typing.mp3",
+    "wind-chimes.mp3"
+  ]
+  $: place_label = selected_place?.PlaceDescriptions[0].display_label;
+  $: serieses = seriesvalues.data.serieses.map((m,i) => {
+    let seriesInfo = {
+      id: m.id,
+      min: Math.min(...m.SeriesValues.map(d => +d.value)),
+      max: Math.max(...m.SeriesValues.map(d => +d.value)),
+      data: m.SeriesValues.map(d => ({
+          place_id: d.place_id,
+          value: +d.value,
+          trend: d.trend,
+          date: DateTime.fromMillis(d.date, {zone: "GMT"})
+      })),
+      soundfile: sounds[i] // instead of an index here we can make sounds an object and use series_id as the key
+      // soundfile: sounds[m.id]
+    };
+    return seriesInfo;
+  })
+
+  // async function to compile data from all the disparate files
+  // for chosen place and serieses selected
+  async function compileData(selected_place:any, series_selection:string[]) {
+    let overlayData = series_selection.map(s => {
+      let seriesInfo = serieses.find(f => f.id === s)
+      let audioData = getAudioData(`/sounds/${seriesInfo?.soundfile}`);
+      let placeData = seriesInfo?.data.find(f => f.place_id === selected_place.id)
+      let seriesDescription = seriesDescriptions.data.serieses.find(f => f.id === s)
+      return ({
+        series_id: s,
+        min: seriesInfo?.min,
+        max: seriesInfo?.max,
+        place_value: placeData?.value,
+        place_trend: placeData?.trend,
+        date: placeData?.date,
+        trend_scalar: seriesDescription?.Metric.trend_scalar,
+        value_format: seriesDescription?.value_format,
+        display_axis_secondary: seriesDescription?.SeriesDescriptions[0].display_axis_secondary,
+        sound: audioData,
+      })
+    })
+    return overlayData;
+  }
+  $: overlayData = compileData(selected_place, series_selection);
+
+  // function to insure a checkbox is not clickable 
+  // if there's no data to load for the selected city
+  function disableCheckbox(place_id:string, series_id:string) {
+    let seriesInfo = serieses.find(f => f.id === series_id)
+    let placeData = seriesInfo?.data.find(f => f.place_id === place_id)
+    if (placeData) {
+      return false
+    } else {
+      return true
+    }
+  }
+  function processTrend(scalar: number, trend: number) {
+    let trendText = undefined;
+    let scalarText = undefined;
+    switch (trend) {
+      case 2:
+        trendText = "Improving Rapidly"        
+        break;
+      case 1: 
+        trendText = "Improving Slowly"
+        break;
+      case 0:
+        trendText = "Not Changing"
+        break;
+      case -1: 
+        trendText = "Worsening Slowly"
+        break;
+      case -2:
+        trendText = "Worsening Rapidly"
+    }
+    switch (scalar) {
+      case -1:
+        scalarText = "Lower"
+        break;
+      case 1: 
+        scalarText = "Higher"
+    }
+    if (scalarText !== undefined && trendText !== undefined) return `${scalarText} numbers mean improvement and this metric is ${trendText}`;
+  }
+  
+  // erie code goes here
+  
+  let stream = new Erie.Stream();
+  
+  // skip narration
+  stream.config.set('skipStartSpeech', true);
+  stream.config.set('skipScaleSpeech', true);
+  stream.config.set('skipStartPlaySpeech', true);
+  stream.config.set('skipFinishSpeech', true);
+  stream.config.set('skipStopSpeech', true);
+
+  async function playSound(soundData:any) {
+    console.log(soundData)
+    if (stopped === false) {
+      stopped = true
+    } else {
+      stopped = false
+    }
+  }
 </script>
 
 <h1>What does a city's data sound like?</h1>
@@ -13,7 +132,7 @@
 </p>
 
 <select
-		bind:value={selected}
+		bind:value={selected_place}
 >
   <option value={undefined}>Choose a place...</option>
   {#each places.data.places as place}
@@ -22,6 +141,100 @@
     </option>
   {/each}
 </select>
-{#if selected}
-  <p>Listen to <a {href}>{label}</a>.</p>
+{#if selected_place}
+  <p>Listen to {place_label}:</p>
+  <div class="col-2">
+    {#if seriesDescriptions}
+    <div id="serieses">
+      {#each seriesDescriptions.data.serieses as series}
+        <div class={disableCheckbox(selected_place.id, series.id) 
+          ? "disabled" 
+          : ""}
+        >
+          <label>
+            <input type="checkbox" bind:group={series_selection} value={series.id} disabled={disableCheckbox(selected_place.id, series.id)} /> 
+            {series.SeriesDescriptions[0].display_axis_primary}
+          </label>
+        </div>
+      {/each}
+    </div>
+    {/if}  
+    <div id="waveform">
+      {#if series_selection.length > 0}
+      {#await overlayData}
+        <div>...loading data</div>
+      {:then soundData}
+      <div>
+        <button 
+          id="play-button"
+          on:click={async(e) => await playSound(soundData)}
+          >
+          {#if stopped}
+            Play
+          {:else}
+            Stop
+          {/if}
+        </button>
+      </div>
+
+      <svg width=400 height=200>
+        <defs>
+          <linearGradient id="lgrad" x1="0%" y1="50%" x2="100%" y2="50%" >
+              <stop offset="0%" style="stop-color:rgb(0,255,10);stop-opacity:1.00" />
+              <stop offset="25%" style="stop-color:rgb(0,188,212);stop-opacity:0.70" />
+              <stop offset="50%" style="stop-color:rgb(238,130,238);stop-opacity:1.00" />
+              <stop offset="75%" style="stop-color:rgb(103,58,183);stop-opacity:0.70" />
+              <stop offset="100%" style="stop-color:rgb(233,30,99);stop-opacity:1.00" />
+          </linearGradient>
+        </defs>
+        {#each soundData as sd}
+        {#await sd.sound}
+          <g />
+        {:then sound} 
+          <path style="fill:none; stroke-width: 3;  stroke:url(#lgrad)">
+            <animate
+                attributeName="d"
+                dur="2.54s"
+                repeatCount="indefinite"
+                calcMode="linear"
+                values={linearPath(sound, { 
+                    samples: 100, type: 'steps', top: 20, animation: true
+                  }
+                )}
+            />
+          </path>
+        {/await}
+        {/each}
+      </svg>
+      <div>
+        {#each soundData as sd}
+        <p>{format(sd.value_format)(sd.place_value)} {sd.display_axis_secondary || ""}</p>
+        {#if sd.trend_scalar && sd.place_trend}
+          <p>{processTrend(+sd.trend_scalar, +sd.place_trend)}</p>
+        {/if}
+        {/each}
+      </div>
+      {:catch error}
+        <div>error generating wave</div>
+      {/await}
+
+      {/if}
+    </div>
+  </div>
 {/if}
+
+<style>
+  .disabled {
+    color: lightgray;
+  }
+  .col-2 {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+  }
+  #serieses {
+    max-height: 75vh;
+    overflow: scroll;
+    padding: 10px;
+  }
+</style>
